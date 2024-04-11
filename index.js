f518381c719e9ac157a2c24206c7c58c3d58d6d2
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2024-04-11 23:42:48 +0530

    short url

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const mongoose=require('mongoose');
 const {connecttomongodb}=require("./connect");
 const urlRoute=require('./routes/url');
+const URL=require('./modules/url');
 const app=express();
 
 connecttomongodb('mongodb://localhost:27017/short-url')
@@ -11,7 +12,19 @@ const port =8001;
 
 app.use(express.json());
 app.use("/url",urlRoute);
+app.get('/:shortId',async (req,res)=>{
+    const shortId=req.params.shortId;
+   const entry=await URL.findOneAndUpdate({
+        shortId
+    },{$push:{
+      visitHistory:{
+        timestamp:Date.now(),
+      }
+    },
 
+    });
+res.redirect(entry.redirectURL);
+});
 
 
 app.listen(port,()=>console.log("server started"));
